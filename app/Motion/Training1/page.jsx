28daf46de3700e252e23be0aff3002fa9ca293b9
tsurubaso <tsurubaso@hotmail.com>
2025-12-27 23:49:23 +0900

    update motion page

--- app/Motion/Training1/page.jsx
+++ app/Motion/Training1/page.jsx
@@ -13,31 +13,9 @@ export default function Training1() {
 
 
 
-        <motion.div
-            style={box}
-            /**
-             * Setting the initial keyframe to "null" will use
-             * the current value to allow for interruptable keyframes.
-             */
-            whileHover={{
-                scale: [null, 1.1, 1.6],
-                transition: {
-                    duration: 0.5,
-                    times: [0, 0.6, 1],
-                    ease: ["easeInOut", "easeOut"],
-                },
-            }}
-            transition={{
-                duration: 0.3,
-                ease: "easeOut",
-            }}
-        />
     
 
-
-/**
- * ==============   Styles   ================
- */
+        <div style={box} className="hover:scale-125 transition-transform duration-900"></div>
 
 
 
